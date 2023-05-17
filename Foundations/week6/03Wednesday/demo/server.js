c5394a5fb7d657ba0e8b6b4baebc68c354c2e1b8
ryanwebb24 <ryanwebb2444@gmail.com>
2023-05-17 12:24:51 -0600

    serves frontend with express.static instead

--- Foundations/week6/03Wednesday/demo/server.js
+++ Foundations/week6/03Wednesday/demo/server.js
@@ -4,16 +4,7 @@ app.use(express.json())
 require("dotenv").config()
 const { SERVER_PORT } = process.env
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + "/public/index.html")
-})
-app.get('/styles', (req,res) => {
-    res.sendFile(__dirname + "/public/styles.css")
-})
-app.get('/main', (req,res) => {
-    res.sendFile(__dirname + "/public/main.js")
-})
-
+app.use(express.static(__dirname + "/public"))
 
 app.listen(SERVER_PORT, () => {
     console.log(`server up on ${SERVER_PORT}`)
