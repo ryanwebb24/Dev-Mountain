d2195050021fdae6d7ac0157249ebf43f1ca6838
ryanwebb24 <ryanwebb2444@gmail.com>
2023-06-21 17:45:29 -0600

    Finished Project with all fixed code

--- Specs/Week3/project/src/components/detailComponents/DetailScreen.jsx
+++ Specs/Week3/project/src/components/detailComponents/DetailScreen.jsx
@@ -2,34 +2,48 @@ import React from 'react'
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./DetailScreen.css"
+import { VscLoading } from "react-icons/vsc"
 
 const DetailScreen = () => {  
   const {id} = useParams()
   const [recipe, setRecipe] = useState([])
-  function getData(){
-    axios.get(`https://recipes.devmountain.com/recipes/${id}`)
+  let data = null
+  const getData = async () =>{
+    await axios.get(`https://recipes.devmountain.com/recipes/${id}`)
       .then((res) => setRecipe(res.data))
       .catch((err) => console.log(err))
   }
   useEffect(()=>{
     getData()
   },[])
-  console.log(recipe)
+  if (recipe.length !== 0) {
+    data = 
+      <section className="details-page">
+        <div className="title-content">
+          <h2 className="title">{recipe.recipe_name}</h2>
+          <img src={recipe.image_url} alt="No Image" className="food-img"/>
+        </div>
+        <div className="full-recipe">
+          <div className="recipe-details">
+            <h2>Recipe</h2>
+            <p>Prep Time: {recipe.prep_time}</p>
+            <p>Cook Time: {recipe.cook_time}</p>
+            <p>Serves: {recipe.serves}</p>
+            <h2>Ingredients</h2>
+            {recipe.ingredients.map(ing => {return <p>{ing.quantity} {ing.ingredient}</p>})}
+          </div>
+          <div className="instructions">
+            <h2>Instructions</h2>
+            {recipe.instructions.replaceAll('"', '').split("\\n").map(step => <p>{step}</p>)}
+          </div>
+        </div>
+      </section>
+  }
   return (
-    <section>
-      <div className="title-content">
-        <h2 className="title">{recipe.recipe_name}</h2>
-        <img src={recipe.image_url} alt="food" />
-      </div>
-      <div className="recipe">
-        <h2>Recipe</h2>
-        <p></p>
-      </div>
-      <div className="instructions">
-        <h2>Instructions</h2>
-        <p>{recipe.instructions}</p>
-      </div>
-    </section>
+    <div className="details">
+      {data ? data : <VscLoading className="loading"/> }
+    </div>
   );
 };
 
--- Specs/Week3/project/src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ Specs/Week3/project/src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,12 +1,155 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import { Formik } from "formik"
+import axios from "axios"
+import "./NewRecipeScreen.css"
 
 const NewRecipeScreen = () => {
-  return (
-    <section>
-      <h1>Tell us about your Recipe!</h1>
-      {/* Here you will have a large form. Be prepared, part 4 will have you build this form in detail, and part 5 will have you style it. Do your best! */}
-    </section>
-  );
-};
+  const [ingredients, setIngredients] = useState([])
+  const [name, setName] = useState("")
+  const [quantity, setQuantity] = useState("")
 
-export default NewRecipeScreen;
+  const initialValues = {
+    type: "",
+    recipeName: "",
+    imageURL: "",
+    prepTime: "",
+    cookTime: "",
+    serves: "",
+    ingredients: [],
+    instructions: "",
+  }
+  const onSubmit = (values) => {
+    values.ingredients = ingredients
+    axios
+      .post("https://recipes.devmountain.com/recipes", values)
+      .then((res) => console.log("it worked"))
+      .catch((err) => console.log(err))
+  }
+
+  const addIngredient = () => {
+    setIngredients((prevValue) => [...prevValue, { name, quantity }])
+    setName("")
+    setQuantity("")
+  }
+  return (
+    <section className="recipe-screen">
+      <h1>Tell us about your Recipe!</h1>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ values, handleChange, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <div className="name-image">
+              <input
+                type="text"
+                placeholder="Recipe Name"
+                value={values.recipeName}
+                onChange={handleChange}
+                name="recipeName"
+              />
+              <input
+                type="text"
+                placeholder="Image URL"
+                value={values.imageURL}
+                onChange={handleChange}
+                name="imageURL"
+              />
+            </div>
+            <div className="type-radio">
+              <div>
+                <input
+                  type="radio"
+                  id="cook"
+                  value="cook"
+                  onChange={handleChange}
+                  name="type"
+                />
+                <label htmlFor="cook">Cook</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="bake"
+                  value="bake"
+                  onChange={handleChange}
+                  name="type"
+                />
+                <label htmlFor="bake">Bake</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="drink"
+                  value="drink"
+                  onChange={handleChange}
+                  name="type"
+                />
+                <label htmlFor="drink">Drink</label>
+              </div>
+            </div>
+            <div className="serve-time">
+              <input
+                type="text"
+                placeholder="Prep Time"
+                value={values.prepTime}
+                onChange={handleChange}
+                name="prepTime"
+              />
+              <input
+                type="text"
+                placeholder="Cook Time"
+                value={values.cookTime}
+                onChange={handleChange}
+                name="cookTime"
+              />
+              <input
+                type="text"
+                placeholder="Serves"
+                value={values.serves}
+                onChange={handleChange}
+                name="serves"
+              />
+            </div>
+            <div className="ingredients">
+              <div className="ingredients-text">
+                <input
+                  type="text"
+                  placeholder="Ingredients"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Quantity"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="ingredients-container">
+                {ingredients.map((ing) => (
+                  <p>
+                    {ing.quantity} {ing.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+            <button type="button" onClick={addIngredient} className="add-btn">
+              Add Another
+            </button>
+            <div className="text-area">
+              <textarea
+                placeholder="Instructions"
+                value={values.instructions}
+                onChange={handleChange}
+                name="instructions"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn">
+              Save
+            </button>
+          </form>
+        )}
+      </Formik>
+    </section>
+  )
+}
+
+export default NewRecipeScreen
